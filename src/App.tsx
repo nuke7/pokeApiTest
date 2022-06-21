1af66171f7c01e1d02728d14cf5f4109e4c7ccba
Mario Saly <salymario03@gmail.com>
2022-06-21 22:18:44 +0200

    Fix eslint and tsconfig issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import MainPage from "./pages/MainPage";
+import './App.css';
+
+import MainPage from './pages/MainPage';
 
 function App() {
   return <MainPage />;
