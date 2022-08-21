92c9c4d29ac864d18016d119897e9874000dd974
Marton Gombos <marton.gombos@yahoo.com>
2022-08-21 23:09:18 +0200

    feat: :tada: fetching from the pokeApi and mapping

---
+++ src/pages/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>My Pokemon Shop</h1>
+    </div>
+  );
+}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,58 @@
+/* eslint-disable max-len */
+// eslint disable no-unused-vars
+import { useEffect, useState } from 'react';
+
+import Header from './Header';
+
+type PokeData = {
+  name: string;
+  weight: number;
+  photo: string;
+};
+
+interface IPokemon {
+  name: string;
+  url: string;
+}
+
 export default function MainPage() {
-  return <div>Hello</div>;
+  const [pokeData, setPokeData] = useState<PokeData[] | null >();
+
+  const pokemonFetch = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+    const data = await response.json();
+    const urls = data.results.map((pokemon: IPokemon) => pokemon.url);
+    const promises = await Promise.all(
+      urls.map(async (url: string) => {
+        const resp = await (await fetch(url)).json();
+        return { name: resp.name, weight: resp.weight, photo: resp.sprites.front_default };
+      }),
+    );
+    setPokeData(promises);
+  };
+
+  useEffect(() => {
+    pokemonFetch();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div>
+        {pokeData && pokeData.map((pokemon, key) => (
+          <p key={`${Math.random() * 1000 + key}`}>
+            {pokemon.name}
+            <p>
+              Price:
+              {' '}
+              {pokemon.weight * 100}
+            </p>
+          </p>
+        ))}
+        {' '}
+
+      </div>
+    </div>
+  );
 }
